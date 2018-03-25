cdf5ebf614a078b551c460f2e426cbc3043b7c80
lcorozco <lcorozco10@gmail.com>
2018-03-25 12:07:21 -0600

    Update kotlin version
    Create login activity with LinearLayout

--- app/src/main/java/com/lcorozco10/tvg/HomeActivity.kt
+++ app/src/main/java/com/lcorozco10/tvg/HomeActivity.kt
@@ -2,11 +2,22 @@ package com.lcorozco10.tvg
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_home.view.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        var textInfo:TextView = findViewById(R.id.textViewInfo)
+        textInfo.setText("Hola Developer")
+
+        var btnLogin:Button = findViewById(R.id.btnLogin)
+        btnLogin.setOnClickListener{
+            textInfo.setText("Clicked login button")
+        }
     }
 }

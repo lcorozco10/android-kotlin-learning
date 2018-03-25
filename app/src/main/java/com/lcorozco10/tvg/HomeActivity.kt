1816506c34ac9593d9dde2dc87e4e555e4f80995
lcorozco <lcorozco10@gmail.com>
2018-03-25 14:20:02 -0600

    Add principal activity
    testing Button and TextEdit components

--- app/src/main/java/com/lcorozco10/tvg/HomeActivity.kt
+++ app/src/main/java/com/lcorozco10/tvg/HomeActivity.kt
@@ -1,23 +1,36 @@
 package com.lcorozco10.tvg
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_home.view.*
+import kotlinx.android.synthetic.main.activity_principal.*
 
 class HomeActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         var textInfo:TextView = findViewById(R.id.textViewInfo)
-        textInfo.setText("Hola Developer")
-
+        var edtUserName:EditText =  findViewById(R.id.edtUserName)
+        var edtPassword:EditText =  findViewById(R.id.edtPassword)
         var btnLogin:Button = findViewById(R.id.btnLogin)
+        textInfo.text = "Welcome to TVG app"
+
+
+
         btnLogin.setOnClickListener{
-            textInfo.setText("Clicked login button")
+            if (edtUserName.text.toString() == "lcorozco" && edtPassword.text.toString() == "123456" ) {
+                val intent = Intent( applicationContext, PrincipalActivity::class.java)
+                startActivity(intent);
+            }else{
+                textInfo.text = "UserName ${edtUserName.text} is invalid"
+            }
         }
     }
 }

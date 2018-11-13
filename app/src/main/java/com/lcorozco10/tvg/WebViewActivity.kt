a46be43b8a9327459c72c7adfa8d48671437ea1d
Luis <lcorozco10@gmail.com>
2018-11-12 23:50:23 -0600

    Testing webView

---
+++ app/src/main/java/com/lcorozco10/tvg/WebViewActivity.kt
@@ -0,0 +1,54 @@
+package com.lcorozco10.tvg
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class WebViewActivity : AppCompatActivity() {
+
+    lateinit var webViewSample:WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+
+        // Here, thisActivity is the current activity
+        val permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_CALENDAR)
+
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.INTERNET)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.INTERNET),
+                        permissionCheck)
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+
+        webViewSample = findViewById(R.id.webViewSample)
+        webViewSample.loadUrl("https://google.com.ni")
+        )
+    }
+}
